c2b137e7ff98562a15e16c6dae5526192ee07a0e
jbuchholzer-rrc <jbuchholzer@academic.rrc.ca>
2026-01-30 14:49:06 -0600

    Create shared state in App.tsx

--- city-transit/src/App.tsx
+++ city-transit/src/App.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import TripPlanner from './TripPlanner'
 
 function App() {
-  return <TripPlanner />
+  // Shared state managed at top level
+  const [selectedFrom, setSelectedFrom] = useState('')
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<TripPlanner selectedFrom={selectedFrom} setSelectedFrom={setSelectedFrom} />}
+      />
+    </Routes>
+  )
 }
 
 export default App
--- city-transit/src/TripPlanner.tsx
+++ city-transit/src/TripPlanner.tsx
@@ -6,23 +6,28 @@
 
 
 
-function TripPlanner() {
+type TripPlannerProps = {
+  selectedFrom: string
+  setSelectedFrom: (value: string) => void
+}
+
+function TripPlanner({ selectedFrom, setSelectedFrom }: TripPlannerProps) {
   return (
     <div>
       {/* Header/Title */}
       <h2>Trip Planner</h2>
 
       {/* From Option */}
-      <select>
-        <option>From</option>
-        <option>Portage & Main</option>
-        <option>The Forks</option>
-        <option>Polo Park</option>
-        <option>St Vital Centre</option>
-        <option>U of M</option>
-        <option>Osborne Village</option>
-        <option>HSC</option>
-        <option>Kildonan Place</option>
+      <select value={selectedFrom} onChange={(e) => setSelectedFrom(e.target.value)}>
+        <option value="">From</option>
+        <option value="Portage & Main">Portage & Main</option>
+        <option value="The Forks">The Forks</option>
+        <option value="Polo Park">Polo Park</option>
+        <option value="St Vital Centre">St Vital Centre</option>
+        <option value="U of M">U of M</option>
+        <option value="Osborne Village">Osborne Village</option>
+        <option value="HSC">HSC</option>
+        <option value="Kildonan Place">Kildonan Place</option>
       </select>
 
       {/* To Option */}
