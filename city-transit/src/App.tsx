5791d7365c82e9e8cd8f9a15b625a00764f2c9ad
khush735 <khushpatel732005@gmail.com>
2026-01-30 14:35:17 -0600

    feat: Implement routing and context for bus route management with new components

--- city-transit/src/App.tsx
+++ city-transit/src/App.tsx
@@ -1,24 +1,21 @@
 import "./App.css";
-
-import BusRouteMap from "./components/bus-route-map/BusRouteMap";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import BusRouteMapPage from './pages/BusRouteMapPage';
+import { StopsProvider } from './context/StopsContext';
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1>City Transit Planner</h1>
-      </header>
-
-      <main>
-        
-        <BusRouteMap />
-       
-      </main>
-
-      <footer className="app-footer">
-        <p>Team Members: Khush, Harsh, Jack</p>
-      </footer>
-    </div>
+    <StopsProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/bus-route-map" element={<BusRouteMapPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </StopsProvider>
   );
 }
 
