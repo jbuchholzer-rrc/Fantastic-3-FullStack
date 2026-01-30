b9ae367e9400015ff8cee6bed06fc35c921e5c4f
jbuchholzer-rrc <jbuchholzer@academic.rrc.ca>
2026-01-30 14:53:57 -0600

    update TripPlanner with requirments I.2 and I.3 and add component

--- city-transit/src/TripPlanner.tsx
+++ city-transit/src/TripPlanner.tsx
@@ -4,7 +4,7 @@
  * @Description Basic Component: Trip planner for Transit WebApp
  */
 
-
+import { useState } from 'react'
 
 type TripPlannerProps = {
   selectedFrom: string
@@ -12,48 +12,70 @@ type TripPlannerProps = {
 }
 
 function TripPlanner({ selectedFrom, setSelectedFrom }: TripPlannerProps) {
+  const [selectedTo, setSelectedTo] = useState('')
+  const [savedTrips, setSavedTrips] = useState<string[]>([])
+
+  // add trip to saved list
+  function handleSaveTrip() {
+    if (selectedFrom && selectedTo) {
+      const trip = selectedFrom + ' to ' + selectedTo
+      setSavedTrips([...savedTrips, trip])
+    }
+  }
+
+  // remove trip from list
+  function handleRemoveTrip(index: number) {
+    const newTrips = savedTrips.filter((_, i) => i !== index)
+    setSavedTrips(newTrips)
+  }
+
   return (
     <div>
-      {/* Header/Title */}
       <h2>Trip Planner</h2>
 
-      {/* From Option */}
-      <select value={selectedFrom} onChange={(e) => setSelectedFrom(e.target.value)}>
-        <option value="">From</option>
-        <option value="Portage & Main">Portage & Main</option>
-        <option value="The Forks">The Forks</option>
-        <option value="Polo Park">Polo Park</option>
-        <option value="St Vital Centre">St Vital Centre</option>
-        <option value="U of M">U of M</option>
-        <option value="Osborne Village">Osborne Village</option>
-        <option value="HSC">HSC</option>
-        <option value="Kildonan Place">Kildonan Place</option>
-      </select>
+      {/* Trip Form */}
+      <div className="card">
+        <select value={selectedFrom} onChange={(e) => setSelectedFrom(e.target.value)}>
+          <option value="">From</option>
+          <option value="Portage & Main">Portage & Main</option>
+          <option value="The Forks">The Forks</option>
+          <option value="Polo Park">Polo Park</option>
+          <option value="St Vital Centre">St Vital Centre</option>
+          <option value="U of M">U of M</option>
+          <option value="Osborne Village">Osborne Village</option>
+          <option value="HSC">HSC</option>
+          <option value="Kildonan Place">Kildonan Place</option>
+        </select>
 
-      {/* To Option */}
-      <select>
-        <option>To</option> 
-        <option>Portage & Main</option>
-        <option>The Forks</option>
-        <option>Polo Park</option>
-        <option>St Vital Centre</option>
-        <option>U of M</option>
-        <option>Osborne Village</option>
-        <option>HSC</option>
-        <option>Kildonan Place</option>
-      </select>
+        <select value={selectedTo} onChange={(e) => setSelectedTo(e.target.value)}>
+          <option value="">To</option>
+          <option value="Portage & Main">Portage & Main</option>
+          <option value="The Forks">The Forks</option>
+          <option value="Polo Park">Polo Park</option>
+          <option value="St Vital Centre">St Vital Centre</option>
+          <option value="U of M">U of M</option>
+          <option value="Osborne Village">Osborne Village</option>
+          <option value="HSC">HSC</option>
+          <option value="Kildonan Place">Kildonan Place</option>
+        </select>
 
-      {/* Routes */}
-      <h3>Route Options</h3> 
-      <ul>
-        <li>Blue Line - 18 min</li>
-        <li>Route 60 - 25 min</li>
-        <li>Route 16 - 32 min</li>
-        <li>RRC Polytech - 3000000 min</li>
-      </ul>
+        <br /><br />
+        <button onClick={handleSaveTrip}>Save Trip</button>
+      </div>
 
-      {/* Mock Button */}
-      <button>Plan Trip</button>  
+      {/* Saved Trips List */}
+      <h3>Saved Trips</h3>
+      {savedTrips.length === 0 ? (
+        <p>No saved trips yet</p>
+      ) : (
+        <ul>
+          {savedTrips.map((trip, index) => (
+            <li key={index}>
+              {trip} <button onClick={() => handleRemoveTrip(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
