81fc8b3dbc4d3e13a68b55d1b700e04ca4a7d90e
jbuchholzer-rrc <jbuchholzer@academic.rrc.ca>
2026-03-30 16:36:23 -0500

    Fix Leaflet marker icons not showing on Vercel

--- city-transit/src/main.tsx
+++ city-transit/src/main.tsx
@@ -1,10 +1,22 @@
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './App.css'
 import App from './App.tsx'
+
+// fix leaflet marker icons not loading in production builds
+// vite changes the asset paths so we need to set them manually
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
